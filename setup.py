2652b5839069fafc733feee496cb16b17b98f116
Allison Thackston <allison@lyonthackston.com>
2020-05-06 07:05:12 +0000

    Releasing version v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='mkdocs-simple-plugin',
-    version='0.0.1',
+    version='0.0.2',
     description='Plugin for adding simple wiki site creation from markdown files interspersed within your code with MkDocs.',
     long_description=long_description,
     long_description_content_type="text/markdown",

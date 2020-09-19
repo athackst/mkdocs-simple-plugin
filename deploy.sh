6a7fb8e74177c6926211cde6e25e3abc07a2406a
Allison Thackston <allison@allisonthackston.com>
2020-09-19 18:34:51 +0000

    Added workflow to test the action

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,6 @@ git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 git fetch origin ${BRANCH_NAME}:${BRANCH_NAME}
 
-mkdocs gh-deploy -b ${BRANCH_NAME}
+if [ -z $DRY_RUN ]; then
+    mkdocs gh-deploy -b ${BRANCH_NAME}
+fi

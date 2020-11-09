d02154f9e00f61d3c87721b5bb9ebb7ae22a800a
athackst <athackst@users.noreply.github.com>
2020-11-09 06:53:34 +0000

    releasing v0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='mkdocs-simple-plugin',
-    version='0.2',
+    version='0.2.1',
     description='Plugin for adding simple wiki site creation from markdown files interspersed within your code with MkDocs.',
     long_description=long_description,
     long_description_content_type="text/markdown",

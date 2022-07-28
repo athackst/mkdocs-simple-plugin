8dfe97bd860a5e519edbbb039b337663cc7240da
Allison Thackston <allison@allisonthackston.com>
2022-07-28 05:54:06 +0000

    Update linting to use setup.cfg file

--- tests/run_linters.sh
+++ tests/run_linters.sh
@@ -17,11 +17,10 @@ pip install flake8 pydocstyle
 #
 # ```bash
 echo "Running flake8 linter -------->"
-flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics --exclude=setup.py
-flake8 . --count --exit-zero --max-complexity=10 --max-line-length=80 --statistics --exclude=setup.py
+flake8 --count .
 
 echo "Running pydocstyle"
-pydocstyle --count --convention=google --add-ignore=D415,D107 .
+pydocstyle --count .
 # ```
 # </details>
 # /md

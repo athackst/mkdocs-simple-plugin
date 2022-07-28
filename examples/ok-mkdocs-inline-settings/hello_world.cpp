733fbede9b5ecfd08f494778d85edf920228fcf0
Allison Thackston <allison@allisonthackston.com>
2022-07-27 22:44:28 -0700

    Add stop inline parameter. (#350)

--- examples/ok-mkdocs-inline-settings/hello_world.cpp
+++ examples/ok-mkdocs-inline-settings/hello_world.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
 int main() {
-  /** md file=main.md trim=2
+  /** md file=main.md trim=2 stop="end-here"
   # Hello world
   
   This is the main function for hello world.
   It outputs "Hello World!" to the screen.
+
+  end-here
+  This shouldn't show up!
   **/
     std::cout << "Hello World!";
     return 0;

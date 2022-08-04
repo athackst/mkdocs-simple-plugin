4228e821d6a87a2bcbb9572b2c5b994e44286991
Allison Thackston <allison@allisonthackston.com>
2022-08-03 23:43:09 -0700

    Separate default version and add push input option to action

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -2,6 +2,7 @@
 
 git config --global --add safe.directory /github/workspace
 
+echo "Building docs"
 mkdocs_simple_gen --config-file ${INPUT_CONFIG:-'mkdocs.yml'}
 mkdocs build
 
@@ -10,7 +11,12 @@ git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 git fetch origin ${INPUT_PUBLISH_BRANCH} --depth=1
 
-if [ -z $DRY_RUN ]; then
-    mike set-default ${INPUT_DEFAULT_VERSION}
+if [ ${INPUT_PUSH} ]; then
     mike deploy -p -u -b ${INPUT_PUBLISH_BRANCH} ${INPUT_VERSION}
+    echo "Deploying ${INPUT_VERSION} to ${INPUT_PUBLISH_BRANCH}"
+
+    if [ ${INPUT_DEFAULT_VERSION} ]; then 
+        mike set-default -p -b ${INPUT_PUBLISH_BRANCH} ${INPUT_DEFAULT_VERSION}
+        echo "Setting default version to ${INPUT_DEFAULT_VERSION}"
+    fi
 fi

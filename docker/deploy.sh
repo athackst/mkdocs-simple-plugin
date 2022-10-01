c1444cf3bee228d3657c39bcc38bfb9030dec35c
Allison Thackston <allison@allisonthackston.com>
2022-10-01 05:49:34 +0000

    Fix deploy script handling of custom config file

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-
 set -e
+
 UNSET='\033[0m'
 RED='\033[00;31m'
 GREEN='\033[00;32m'
 YELLOW='\033[00;33m'
 CYAN='\033[00;36m'
 
-
 git config --global --add safe.directory /github/workspace
 
 echo -e "${CYAN}Building docs${UNSET}"
-mkdocs_simple_gen --config-file ${INPUT_CONFIG:-'mkdocs.yml'}
-mkdocs build
+mkdocs_simple_gen --config-file ${INPUT_CONFIG}
 
 if [[ "${INPUT_PUSH}" == "1" || "${INPUT_PUSH,,}" == "true" ]]; then
     git config --global user.name "${GITHUB_ACTOR}"
@@ -22,14 +20,16 @@ if [[ "${INPUT_PUSH}" == "1" || "${INPUT_PUSH,,}" == "true" ]]; then
 
     if [ "${INPUT_VERSION}" ]; then
         echo -e "${CYAN}Deploying ${INPUT_VERSION} to ${INPUT_PUBLISH_BRANCH}${UNSET}"
-        mike deploy -p -u -b ${INPUT_PUBLISH_BRANCH} ${INPUT_VERSION}
+        mike deploy --config-file ${INPUT_CONFIG} -p -u -b ${INPUT_PUBLISH_BRANCH} ${INPUT_VERSION}
     else
         echo -e "${CYAN}Deploying docs to ${INPUT_PUBLISH_BRANCH}${UNSET}"
-        mkdocs gh-deploy -b ${INPUT_PUBLISH_BRANCH}
+        mkdocs gh-deploy --config-file ${INPUT_CONFIG} -b ${INPUT_PUBLISH_BRANCH} 
     fi
 
     if [ "${INPUT_DEFAULT_VERSION}" ]; then 
         echo -e "${CYAN}Setting default version to ${INPUT_DEFAULT_VERSION}${UNSET}"
         mike set-default -p -b ${INPUT_PUBLISH_BRANCH} ${INPUT_DEFAULT_VERSION}
     fi
+else 
+    mkdocs build --config-file ${INPUT_CONFIG}
 fi

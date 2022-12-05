a9d8b7c013695659defb1c80a4e08f6f0e83f198
Allison Thackston <allison@allisonthackston.com>
2022-12-05 03:04:05 +0000

    Add setup script

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-pip install -r requirements.txt
 pip install -e .
 # md file="build.snippet" content="^#?\s?(.*)"
 # Building this package requires  using `mkdocs_simple_gen` to generate the site.

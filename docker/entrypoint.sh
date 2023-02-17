a49254e6fbdc2a53ea1747c588af1096cc96aa33
Allison Thackston <allison@allisonthackston.com>
2023-02-17 12:32:31 -0800

    Better support running the docker as a user (#486)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
+git config --global --add safe.directory /docs
+
 if [ -f "requirements.txt" ]; then
-  pip install -r requirements.txt
+  if [ "$UID" -eq 0 ]; then
+    pip install -r requirements.txt
+  else
+    pip install --user -r requirements.txt
+  fi
 fi
 
 exec "$@"

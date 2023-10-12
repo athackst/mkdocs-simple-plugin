26722e0ab5a4c99753b95c3d2eacfdd3c485a8ab
Allison Thackston <allison@allisonthackston.com>
2023-10-12 17:20:03 +0000

    Change devcontainer to use image instead of file

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # md file="setup.snippet" content="^#?\s?(.*)"
 # You will need to have [MKDocs](https://www.mkdocs.org/) installed on your system.
 # I recommend installing it via pip to get the latest version.
-# 
+#
 # ```bash
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
@@ -18,5 +18,5 @@ sudo apt-get install -y --no-install-recommends \
 pip install --upgrade pip
 pip install --user -r requirements.txt
 # ```
-# 
+#
 # /md

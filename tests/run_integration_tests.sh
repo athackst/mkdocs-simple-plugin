604fc930911f2d766e6d191a963cd2b21a130ae8
Allison Thackston <allison@allisonthackston.com>
2023-10-31 11:50:58 -0700

    Update integration tests run script (#609)

--- tests/run_integration_tests.sh
+++ tests/run_integration_tests.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -e
-sudo apt-get install bats
+sudo apt-get install -y bats
 pip install -e .
 pip install mkdocs-macros-plugin mkdocstrings mkdocstrings-python-legacy mkdocs-material
 # md file="integration_tests.snippet" content="^#?\s?(.*)"
 # ### Integration tests
-# 
+#
 # Integration testing allows the plugin to be tested with mkdocs using example
 # configurations.
-# 
+#
 # Integration testing uses bats, install it with
-# 
+#
 # ```bash
 # sudo apt-get install bats
 # ```
 # Then run the tests
-# 
+#
 # ```bash
 # ./tests/run_integration_tests.sh
 # ```
@@ -24,7 +24,7 @@ pip install mkdocs-macros-plugin mkdocstrings mkdocstrings-python-legacy mkdocs-
 #
 # ```bash
 ./examples/gen_readme.py
-./tests/integration_test.bats
+./tests/integration.bats
 # ```
 # </details>
 # /md
